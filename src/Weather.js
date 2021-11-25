4bdfcba43fe206c8068504d34e5afbad38379fc5
maklad42 <maklad42+git@gmail.com>
2021-11-25 23:15:15 +0900

    Adds Loading div to prevent error on first page load. Need to add classes and css to make this a spinner rather than just text. Also, need to change the date strings to formatted dates. 211125

--- src/Weather.js
+++ src/Weather.js
@@ -4,8 +4,10 @@ import './Weather.css';
 const Weather = () => {
   // const [state, setState] = useState([]);
   const [forecast, setForecast] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     const url =
       'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Kawasaki&APPID=a6e7346ac64558f1345eac552358e84e';
     // fetch('https://randomuser.me/api')
@@ -18,37 +20,48 @@ const Weather = () => {
       })
       .then((data) => {
         setForecast(data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching data: ', error);
+        setLoading(false);
       });
   }, []);
 
   return (
     <>
-      <div className="wrapper">
-        {/* <div className="weather">Weather App</div> */}
-        <div className="weather-details">
-          <div className="top">
-            <div className="location">{forecast.name}</div>
-            <div className="date">{forecast.dt}</div>
-          </div>
-          <div className="left">
-            <div className="icon">{forecast.icon}</div>
-            <div className="details">{forecast.weather[0]['main']}</div>
-            <div className="details">{forecast.weather[0]['description']}</div>
-          </div>
-          <div className="right">
-            <div className="temp">{forecast.main.temp}</div>
-            <div className="humidity">{forecast.main.temp_max}</div>
-            <div className="humidity">{forecast.main.temp_min}</div>
-            <div className="humidity">{forecast.main.humidity}</div>
-          </div>
+      {loading ? (
+        <div className="wrapper">
+          <div>Loading...</div>
         </div>
-      </div>
-      <div>
-        <pre>{JSON.stringify(forecast, null, 2)}</pre>
-      </div>
+      ) : (
+        <>
+          <div className="wrapper">
+            <div className="weather-details">
+              <div className="top">
+                <span className="location fltLeft">{forecast.name}</span>
+                <span className="date fltRight">{forecast.dt}</span>
+              </div>
+              <div className="left">
+                <div className="icon">{forecast.icon}</div>
+                <div className="details">{forecast.weather[0]['main']}</div>
+                <div className="details">
+                  {forecast.weather[0]['description']}
+                </div>
+              </div>
+              <div className="right">
+                <div className="temp">{forecast.main.temp}</div>
+                <div className="humidity">{forecast.main.temp_max}</div>
+                <div className="humidity">{forecast.main.temp_min}</div>
+                <div className="humidity">{forecast.main.humidity}</div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <pre>{JSON.stringify(forecast, null, 2)}</pre>
+          </div>
+        </>
+      )}
     </>
   );
 };
